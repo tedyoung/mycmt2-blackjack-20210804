692bf736f1b6ec8098178e1fb8d3a0a844961f7b
Ted M. Young <tedyoung@gmail.com>
2021-08-04 11:49:49 -0700

    Lab #2: Moved display method into Game and cards as String into ConsoleHand.
    Tidied up the code, exposing value() instead of displayValue() and adjusted tests.

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -2,9 +2,6 @@ package com.jitterted.ebp.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class Hand {
     private final List<Card> cards = new ArrayList<>();
@@ -16,7 +13,7 @@ public class Hand {
     public Hand() {
     }
 
-    private int value() {
+    public int value() {
         int handValue = cards
                 .stream()
                 .mapToInt(Card::rankValue)
@@ -43,11 +40,9 @@ public class Hand {
         return value() <= 16;
     }
 
-    void display() {
-        System.out.println(cards.stream()
-                                .map(ConsoleCard::display)
-                                .collect(Collectors.joining(
-                                        ansi().cursorUp(6).cursorRight(1).toString())));
+    // QUERY: "snapshot"
+    public List<Card> cards() {
+        return List.copyOf(cards);
     }
 
     public void drawFrom(Deck deck) {
@@ -66,11 +61,4 @@ public class Hand {
         return hand.value() < value();
     }
 
-    String displayValue() {
-        return String.valueOf(value());
-    }
-
-    public boolean valueEquals(int target) {
-        return value() == target;
-    }
 }
