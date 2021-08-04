dfacace81ad26f17bfa42c2a58d137cf6cc16506
Ted M. Young <tedyoung@gmail.com>
2021-08-04 10:12:57 -0700

    Lab #1: Created Characterization tests for Card display.

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,25 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CardDisplayTest {
+
+    @Test
+    public void displayTenAsString() throws Exception {
+        Card card = new Card(Suit.CLUBS, Rank.TEN);
+
+        assertThat(card.display())
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+
+    @Test
+    public void displayNonTenAsString() throws Exception {
+        Card card = new Card(Suit.HEARTS, Rank.QUEEN);
+
+        assertThat(card.display())
+                .isEqualTo("[31m┌─────────┐[1B[11D│Q        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        Q│[1B[11D└─────────┘");
+    }
+
+}
